485e96de0dac85671bfc58c534074f32fc4f0b8b
Eric Connally <econnally@gmail.com>
2020-03-06 10:47:16 -0500

    incorrect embed tags in image index views
    https://www.pivotaltracker.com/story/show/171381476
    
    * also includes fix in embeddable_content_seeds.rb for im-preview app

--- app/models/concerns/provides_embeddable_content.rb
+++ app/models/concerns/provides_embeddable_content.rb
@@ -15,14 +15,23 @@ module ProvidesEmbeddableContent
     delegate(*EmbeddableModelConfig.attributes, to: self)
   end
 
+  def embeddable_tag_extension
+    try(:file).try(:filename).try(:extension) || embeddable_tag_ext
+  end
+
   def embeddable_tag(opts = {})
-    tag.send embeddable_tag_name, embeddable_tag_attrs(opts)
+    convert_safe_buffer_to_string tag.send embeddable_tag_name,
+                                           embeddable_tag_attrs(opts)
   end
 
   private
 
+  def convert_safe_buffer_to_string(safe_buffer)
+    safe_buffer.to_str
+  end
+
   def embeddable_tag_attrs(opts)
-    { src:  "/#{model_name.route_key}/#{id}.#{embeddable_tag_ext}",
+    { src:  "/#{model_name.route_key}/#{id}.#{embeddable_tag_extension}",
       data: data_attributes(opts) }.compact
   end
 
--- db/embeddable_content/embeddable_content_seeds.rb
+++ db/embeddable_content/embeddable_content_seeds.rb
@@ -1,9 +1,11 @@
-[Embedding, EmbedderConfig, EmbeddableModelConfig].each do |model|
-  model.destroy_all
-  Pathname.new(File.expand_path('./', __dir__)).tap do |dir|
-    dir.join(model.model_name.plural).sub_ext('.json').tap do |file|
-      JSON.parse(file.read).each do |attrs|
-        model.create attrs
+if Rails.env.test?
+  [Embedding, EmbedderConfig, EmbeddableModelConfig].each do |model|
+    model.destroy_all
+    Pathname.new(File.expand_path('./', __dir__)).tap do |dir|
+      dir.join(model.model_name.plural).sub_ext('.json').tap do |file|
+        JSON.parse(file.read).each do |attrs|
+          model.create attrs
+        end
       end
     end
   end
