708f357a2497ce856f7aa0ffae2510acfe5fb65d
dylan dechant <dylan.dechant@gmail.com>
2020-04-23 19:47:30 -0400

    fixes for https://www.pivotaltracker.com/story/show/172445419

--- app/services/embeddable_content/sad_embedded_tags.rb
+++ app/services/embeddable_content/sad_embedded_tags.rb
@@ -1,13 +1,22 @@
 module EmbeddableContent
   class SadEmbeddedTags
-    attr_accessor :sad_related_records, :embedded_models
+    attr_accessor :root_node, :sad_related_records, :embedded_models
 
     def initialize(root_node, embedded_models)
+      @root_node = root_node
       @sad_related_records = SadRelatedRecords.new root_node
       @embedded_models     = embedded_models
     end
 
     def each
+      @root_node.tags.each do |tag|
+        tag.html_attrs.each do |attr|
+          EmbeddedTags.new(tag, attr, embedded_models).each do |info|
+            yield nil, nil, info
+          end
+        end
+      end
+
       sad_related_records.each do |node, node_address, related_record|
         related_record.html_attrs.each do |attr|
           EmbeddedTags.new(related_record, attr, embedded_models).each do |info|
