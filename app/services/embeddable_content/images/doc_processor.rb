2162f975b9f48d9191f256120f24cf3a93cc36cf
Eric Connally <econnally@gmail.com>
2021-05-11 15:17:02 -0400

    RuntimeError: Cannot add sibling to a node with no parent
    https://app.clubhouse.io/illustrative-mathematics/story/659/runtimeerror-cannot-add-sibling-to-a-node-with-no-parent

--- app/services/embeddable_content/images/doc_processor.rb
+++ app/services/embeddable_content/images/doc_processor.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 module EmbeddableContent
   module Images
     class DocProcessor < EmbeddableContent::DocProcessor
       delegate :image_catalog, to: :embedder
+
+      CLASS_IMG_TAG_RELOCATED = 'img-tag-relocated-by-embedder'
 
       private
 
@@ -26,12 +30,19 @@ module EmbeddableContent
         img_tags_inside_p_tags.each do |img_tag|
           move_img_tag_out_from_p_tag img_tag
         end
+        remove_affected_empty_p_tags
+      end
+
+      def remove_affected_empty_p_tags
+        document.css("p.#{CLASS_IMG_TAG_RELOCATED}").each do |p_tag|
+          remove_if_empty p_tag
+        end
       end
 
       def move_img_tag_out_from_p_tag(img_tag)
         img_tag.parent.tap do |p_tag|
           p_tag.add_previous_sibling img_tag
-          remove_if_empty p_tag
+          p_tag.add_class CLASS_IMG_TAG_RELOCATED
         end
       end
 
